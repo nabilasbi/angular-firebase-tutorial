41853434dec38e1b65342d6bed2a9cb5bc345b0c
Nabil Asbi <nabil.asbi@service-now.com>
2014-06-08 18:22:28 +0200

    define the app var in the code to be used

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -8,7 +8,7 @@
  *
  * Main module of the application.
  */
-angular
+var app = angular
   .module('angularFirebaseTutorialApp', [
     'ngAnimate',
     'ngCookies',
@@ -16,8 +16,8 @@ angular
     'ngRoute',
     'ngSanitize',
     'ngTouch'
-  ])
-  .config(function ($routeProvider) {
+  ]);
+  app.config(function ($routeProvider) {
     $routeProvider
       .when('/', {
         templateUrl: 'views/main.html',
