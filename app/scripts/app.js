527c116064817a404bd9420bed4878a77aa7887b
Nabil Asbi <nabil.asbi@service-now.com>
2014-06-08 19:18:31 +0200

    Adding the posts view and Posts Controller

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -19,6 +19,10 @@ var app = angular
   ]);
   app.config(function ($routeProvider) {
     $routeProvider
+      .when('/', {
+        templateUrl: 'views/posts.html',
+        controller: 'PostsCtrl'
+      })
       .when('/about', {
         templateUrl: 'views/about.html',
         controller: 'AboutCtrl'
